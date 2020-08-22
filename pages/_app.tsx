2610c153759c77adba1ab9618c37a8325b02985c
Raghib Hasan <raghibmidhat@gmail.com>
2020-08-22 12:26:00 +0300

    add: styled-components related logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,28 @@
-import App from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { AppProps } from 'next/app'
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+export const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
+)
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+
+//   return { ...appProps }
+// }
+
+export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export const IndexPage = () => <div>IndexPage</div>
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`
+
+export const IndexPage = () => <Title>IndexPage</Title>
 
 export default IndexPage
